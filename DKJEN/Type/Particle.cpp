9a04e2fdcf5c31254efe676c653fe2e3f377ba32
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-01-04 19:05:55 +0900

    1

--- DKJEN/Type/Particle.cpp
+++ DKJEN/Type/Particle.cpp
@@ -2,7 +2,7 @@
 
 void Particle::Initialize()
 {
-
+	InstancingResource = CreateBufferResource(sizeof(TransformationMatrix) * kNumIstance);
 }
 
 void Particle::SRV()
@@ -25,6 +25,31 @@ void Particle::SRV()
 
 void Particle::Draw()
 {
+	Matrix4x4 ProjectionMatrix = MakePerspectiveFovMatrix(0.45f, float(1280.0f / 720.0f), 0.1f, 100.0f);
+	TransformationMatrix* inststacingDet = nullptr;
+	InstancingResource->Map(0, nullptr, reinterpret_cast<void**>(&inststacingDet));
+	for (uint32_t i = 0; i < kNumIstance; ++i)
+	{
+		inststacingDet[i].WVP = MakeIdentity4x4();
+		inststacingDet[i].World = MakeIdentity4x4();
+	}
+	Transform transfom[10];
+	for (uint32_t i = 0; i < kNumIstance; ++i)
+	{
+		transfom[i].scale = { 1.0f,1.0f,1.0f };
+		transfom[i].rotate = { 0.0f,0.0f,0.0f };
+		transfom[i].translate = { i * 0.1f,i * 0.1f,i * 0.1f };
+	}
+
+	for (uint32_t i = 0; i < kNumIstance; ++i)
+	{
+		Matrix4x4 woldMatrix = MakeAffineMatrix(transfom[i].scale, transfom[i].rotate, transfom[i].translate);
+		Matrix4x4 woldViewProMatrix = Multiply(woldMatrix, ProjectionMatrix);
+		inststacingDet[i].WVP = woldViewProMatrix;
+		inststacingDet[i].World = woldMatrix;
+	}
+
+
 	modelData.vertices.push_back({ vertexData.position = {1.0f,1.0f,0.0f,1.0f}, vertexData.texcoord = {0.0f,0.0f},vertexData.normal = {0.0f,0.0f,1.0f} });
 	modelData.vertices.push_back({ vertexData.position = {1.0f,1.0f,0.0f,1.0f}, vertexData.texcoord = {0.0f,0.0f},vertexData.normal = {0.0f,0.0f,1.0f} });
 	modelData.vertices.push_back({ vertexData.position = {1.0f,1.0f,0.0f,1.0f}, vertexData.texcoord = {0.0f,0.0f},vertexData.normal = {0.0f,0.0f,1.0f} });
@@ -33,5 +58,53 @@ void Particle::Draw()
 	modelData.vertices.push_back({ vertexData.position = {1.0f,1.0f,0.0f,1.0f}, vertexData.texcoord = {0.0f,0.0f},vertexData.normal = {0.0f,0.0f,1.0f} });
 	modelData.material.textureFilePath = "./resoures/uvChecker.png";
 
+	ID3D12GraphicsCommandList* commandList = DxCommon::GetInstance()->GetCommandList();
+	PSOProperty pso_ = PaticlePSO::GetInstance()->GetPSO().Texture;
+	commandList->SetGraphicsRootSignature(pso_.rootSignature);
+	commandList->SetPipelineState(pso_.GraphicsPipelineState);//
 
+
+	commandList->IASetVertexBuffers(0, 1, &vertxBufferView);
+
+	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	commandList->SetGraphicsRootDescriptorTable(1, instancingSrvHandleGPU);
+	commandList->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
+	commandList->SetGraphicsRootDescriptorTable(2, tex_.SrvHandleGPU);
+	commandList->SetGraphicsRootConstantBufferView(3, lightResource->GetGPUVirtualAddress());
+
+	commandList->DrawInstanced(UINT(modelData.vertices.size()), inst_, 0, 0);
+
+}
+
+ID3D12Resource* Particle::CreateBufferResource(size_t sizeInbyte)
+{
+	ID3D12Device* device = DxCommon::GetInstance()->GetDevice();
+	ID3D12Resource* RssultResource;
+	//頂点リソース用のヒープの設定
+	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
+
+	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; //UploadHeapを使う
+
+	//頂点リソースの設定
+	D3D12_RESOURCE_DESC ResourceDesc{};
+
+	//バッファリソース。テクスチャの場合はまた別の設定をする
+	ResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	ResourceDesc.Width = sizeInbyte; //リソースのサイズ。今回はvector4を3頂点分
+
+	//バッファの場合はこれらは1にする決まり
+	ResourceDesc.Height = 1;
+	ResourceDesc.DepthOrArraySize = 1;
+	ResourceDesc.MipLevels = 1;
+	ResourceDesc.SampleDesc.Count = 1;
+
+	//バッファの場合はこれにする決まり
+	ResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	HRESULT hr;
+	hr = device->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE,
+		&ResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&RssultResource));
+	assert(SUCCEEDED(hr));
+
+	return RssultResource;
 }
--- DKJEN/Type/Particle.h
+++ DKJEN/Type/Particle.h
@@ -1,5 +1,14 @@
 #pragma once
 #include"Obj3D.h"
+#include"../Math/Math.h"
+#include"../Base/DxCommon.h"
+#include"../Base/SpritePSO.h"
+#include"../Base/TexturePSO.h"
+#include"../Base/LightPSO.h"
+#include"../Base/PaticlePSO.h"
+#include"../Base/ImageLoading.h"
+#include<fstream>
+#include<sstream>
 #include"../Base/ImageLoading.h"
 class Particle
 {
@@ -7,9 +16,15 @@ public:
 	void Initialize();
 	void SRV();
 	void Draw();
+	ID3D12Resource* CreateBufferResource(size_t sizeInbyte);
 private:
 	ModelData modelData;
 	VertexData vertexData;
+
+	ID3D12Resource* lightResource;
+
+	const uint32_t kNumIstance = 10;
+	ComPtr<ID3D12Resource>InstancingResource
 };
 
 
