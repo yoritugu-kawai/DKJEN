7bd5824e813647c2da4a88c88b984d15b0bbde13
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-01-08 00:28:13 +0900

    1

---
+++ seen.cpp
@@ -0,0 +1,24 @@
+#include "seen.h"
+
+void seen::Initialize(TexProeerty  tex)
+{
+
+
+	SpriteTex->Initialize(  tex);
+	SpriteTex2->Initialize(tex);
+}
+
+void seen::Updet()
+{
+
+
+
+
+}
+
+void seen::Draw()
+{
+
+	SpriteTex->Darw();
+	SpriteTex2->Darw();
+}
--- seen.h
+++ seen.h
@@ -1,1 +1,19 @@
 #pragma once
+#include"DKJEN/Type/Sprite.h"
+#include"DKJEN/Base/ImageLoading.h"
+#include"DKJEN/Math/Math.h"
+#include"DKJEN/Type/Obj3D.h"
+#include"DKJEN/Utilipy/rektyk.h"
+#include"DKJEN/Management/FrameManagement.h"
+#include"DKJEN/Management/PSOCopileManagement.h"
+class seen
+{
+public:
+	void Initialize(TexProeerty  tex);
+	void Updet();
+	void Draw();
+
+private:
+	Sprite* SpriteTex;
+	Sprite* SpriteTex2;
+};
