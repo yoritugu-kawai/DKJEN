8b8b49e45ff7a55635d2ddc727e2dbb659c727d2
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-01-22 16:42:03 +0900

    1

--- GameProject/Enemy/Enemy.h
+++ GameProject/Enemy/Enemy.h
@@ -7,7 +7,7 @@ public:
 	void Intiailize(TexProeerty  tex, const std::string& directoryPath, const std::string& filename, Coordinate pos);
 	void Update();
 	void Draw(CameraProjection pro);
-
+	Coordinate GetPos() { return  pos_; };
 private:
 	Obj3D* obj3d_;
 	Coordinate pos_;
