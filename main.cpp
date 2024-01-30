f56baefe39b8a4e95b1df2e65142d77564e1f8e4
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-01-30 12:53:43 +0900

    1

--- main.cpp
+++ main.cpp
@@ -19,7 +19,20 @@
 #include"GameProject/Player/Player.h"
 #include"GameProject/Collder/CollisionManager.h"
 const wchar_t Title[] = { L"ド根性エンジン" };
+float pos(float ax,float x1,float x2,float speed) {
 
+	ax = x1 - x2;
+	if (ax >= -0.1) {
+
+		x2 += speed;
+	}
+	if (ax <= 0.1) {
+
+		x2 -= speed;
+	}
+
+	return x2;
+}
 
 //static D3DResourceLeaker leaker;
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -28,7 +41,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DxCommon::Initialize();
 	ImguiManager::Initialize();
 	Input::Initialize();
-	
+
 	PSOCopileManagement::Set();
 	ImageLoading* imageLoading = new ImageLoading;
 	imageLoading->Initiluze();
@@ -48,8 +61,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Sprite* SpriteTex2 = new Sprite;
 	SpriteTex2->Initialize(texUV);
 
-	/*Obj3D* obj3D = new Obj3D;
-	obj3D->Initialize(texUV,"resource","axis.obj");*/
+	Obj3D* obj3D = new Obj3D;
+	obj3D->Initialize("GameResource", "Player.obj");
+
+	Obj3D* obj3D2 = new Obj3D;
+	obj3D2->Initialize("GameResource", "Player.obj");
 
 
 	//ゲームキャラクター
@@ -66,14 +82,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	enemyTransfrom.color = { 1.0f,1.0f,1.0f,1.0f };
 
 	Player* player_ = new Player;
-	player_->Intiailize( "GameResource", "Player.obj",playerTransfrom);
+	player_->Intiailize("GameResource", "Player.obj", playerTransfrom);
 
 	Enemy* enemy_ = new Enemy();
-	enemy_->Intiailize( "GameResource", "Player.obj", enemyTransfrom);
-	
+	enemy_->Intiailize("GameResource", "Player.obj", enemyTransfrom);
+
 
 	Skydome* skydome_ = new Skydome;
-	skydome_->Intiailize( "GameResource", "CelestialSphere.obj");
+	skydome_->Intiailize("GameResource", "CelestialSphere.obj");
 	//Bullet* bullet_ = new Bullet;
 	//bullet_->Intiailize();
 	CollisionManager* collisionManager_ = new CollisionManager;
@@ -90,15 +106,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	imGuiSprite2.translate = { 50.0f,50.0f,50.0f };
 	imGuiSprite2.color = { 1.0f, 1.0f, 1.0, 1.0f };
 
-	Coordinate imGui3D[1];
-	for (int i = 0; i < 1; i++) {
-		imGui3D[i].scale = { 0.5f, 0.5f, 0.5f };
-		imGui3D[i].rotate = { 0.0f, 0.0f, 0.0f };
-		imGui3D[i].translate = { 0.0f, 0.0f, 0.0f };
-		imGui3D[i].color = { 1.0f,1.0f,1.0f,1.0f };
+	Coordinate imGui3D;
 
-	}
-	
+	imGui3D.scale = { 0.5f, 0.5f, 0.5f };
+	imGui3D.rotate = { 0.0f, 0.0f, 0.0f };
+	imGui3D.translate = { 0.0f, 0.0f, 0.0f };
+	imGui3D.color = { 1.0f,1.0f,1.0f,1.0f };
+
+	Coordinate imGui3D2;
+
+	imGui3D2.scale = { 0.5f, 0.5f, 0.5f };
+	imGui3D2.rotate = { 0.0f, 0.0f, 0.0f };
+	imGui3D2.translate = { 1.0f, 1.0f, 1.0f };
+	imGui3D2.color = { 1.0f,0.0f,0.0f,1.0f };
+	float x = 0;
+	float y = 0;
+	float speed = 0.02f;
 	//　メインループ
 	MSG msg{};
 	while (msg.message != WM_QUIT)
@@ -112,7 +135,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//　ゲーム処理
 		//////
 
-		
+
 		pro.Update();
 
 
@@ -122,7 +145,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		ImGui::SliderFloat3("rotate", &imGuiSprite.rotate.x, -5.0f, 5.0f);
 		ImGui::SliderFloat3("translate", &imGuiSprite.translate.x, -500.0f, 500.0f);
 		ImGui::End();
-		
+
 		SpriteTex->Darw(imGuiSprite.scale, imGuiSprite.rotate, imGuiSprite.translate, imGuiSprite.color);
 
 		ImGui::Begin("Sprite2");
@@ -132,21 +155,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		ImGui::SliderFloat3("translate", &imGuiSprite2.translate.x, -500.0f, 500.0f);
 		ImGui::End();
 		SpriteTex2->Darw(imGuiSprite2.scale, imGuiSprite2.rotate, imGuiSprite2.translate, imGuiSprite2.color);*/
-		
-//#ifdef _DEBUG
-//		ImGui::Begin("a");
-//
-//		ImGui::End();
-//
-//#endiftry
-	
 
+		//#ifdef _DEBUG
+		//		ImGui::Begin("a");
 		//
+		//		ImGui::End();
+		//
+		//#endiftry
+
+
+				//
 		ImGui::Begin("pro");
-	
+
 		ImGui::DragFloat3("translate", &pro.translate.x);
 		ImGui::DragFloat3("rotate", &pro.rotate.x);
-		
+
 		ImGui::End();
 
 
@@ -157,32 +180,71 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		ImGui::SliderFloat3("translate", &imGui3D[0].translate.x, -5.0f, 5.0f);
 		ImGui::End();
 
-		
+
 		obj3D->Draw(imGui3D[0].scale, imGui3D[0].rotate, imGui3D[0].translate, imGui3D[0].color,pro);*/
 		//
 
-		
-		player_->Update();
-		player_->Draw(texPlayer, pro);
-		enemy_->Update(player_);
-		enemy_->Draw(texEnemy, pro);
-		skydome_->Draw(texCelestialSphere, pro);
+		if (Input::GetInstance()->PushKey(DIK_L)) {
+
+			player_->Update();
+			player_->Draw(texPlayer, pro);
+			enemy_->Update(player_);
+			enemy_->Draw(texEnemy, pro);
+			skydome_->Draw(texCelestialSphere, pro);
 
 
-		collisionManager_->CollideClear();
-	   // enemy_->GetBullets();
-		const std::list<Bullet*>& playerBullets = player_->GetBullets();
-		collisionManager_->ColliderPush(player_);
-		collisionManager_->ColliderPush(enemy_);
-		for (Bullet* bullet : playerBullets) {
-			collisionManager_->ColliderPush(bullet);
+			collisionManager_->CollideClear();
+			// enemy_->GetBullets();
+			const std::list<Bullet*>& playerBullets = player_->GetBullets();
+			collisionManager_->ColliderPush(player_);
+			collisionManager_->ColliderPush(enemy_);
+			for (Bullet* bullet : playerBullets) {
+				collisionManager_->ColliderPush(bullet);
+			}
+			/*for (EnemyBullet* bullet : enemyBullets) {
+				collisionManager_->ColliderPush(bullet);
+			}*/
+			collisionManager_->CheckAllCollisions();
+
 		}
-		/*for (EnemyBullet* bullet : enemyBullets) {
-			collisionManager_->ColliderPush(bullet);
-		}*/
-		collisionManager_->CheckAllCollisions();
-	
+
+		if (Input::GetInstance()->PushKey(DIK_A)) {
+			imGui3D.translate.x -= 0.05f;
+		}
+		if (Input::GetInstance()->PushKey(DIK_D)) {
+			imGui3D.translate.x += 0.05f;
+		}
+		if (Input::GetInstance()->PushKey(DIK_S)) {
+			imGui3D.translate.y -= 0.05f;
+		}
+		if (Input::GetInstance()->PushKey(DIK_W)) {
+			imGui3D.translate.y += 0.05f;
+		}
+		pro.translate.z = -10.0f;
+
 		
+		imGui3D2.translate.x=pos(x, imGui3D.translate.x,imGui3D2.translate.x, speed);
+		imGui3D2.translate.y = pos(y, imGui3D.translate.y, imGui3D2.translate.y, speed);
+
+		
+		//imGui3D2.translate.z += 0.05f;
+		/*	imGui3D2.translate.x = x;*/
+		ImGui::Begin("x");
+		ImGui::Text("%f", x);
+		ImGui::End();
+		
+
+
+
+
+		obj3D->Draw(imGui3D.scale, imGui3D.rotate, imGui3D.translate, imGui3D.color, pro, texUV);
+
+
+		obj3D2->Draw(imGui3D2.scale, imGui3D2.rotate, imGui3D2.translate, imGui3D2.color, pro, texUV);
+
+
+
+
 		FrameManagement::EndFrame();
 	}
 
@@ -192,9 +254,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	*************************
 	*/
 	player_->~Player();
-	
+
 	/*delete SpriteTex;
-	
+
 	delete obj3D;
 	*/
 	PSOCopileManagement::Release();
